ceda02194f801d5fab3384d2bba4b29750cc9cad
h89161h <h89161h@yahoo.com.tw>
2017-12-06 00:11:23 +0800

    Add files via upload

---
+++ copyfoldername.m
@@ -0,0 +1,20 @@
+% In SPM preprocessing
+% We make some folders to store the steps of data processing
+% For example, There ara a lot of subfolders (e.g. Sub001,Sub002,....) under FolderToCloned(e.g. Rawdata)
+% And then I want to create a NewFolder(e.g. Preprocesing) with a lot of empty subfolders which have the same names as those in FolderToCloned
+
+%%%%%%%%%%%%%%%%%%%Depend on you needs%%%%%%%%%%%%%%%%%%
+FolderToCloned ='E:\Rawdata'; %%the folder to be cloned
+NewFolder = 'E:\Preprocessing'; %%the folder to be created
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+files = dir(FolderToCloned);
+filenames = {files.name};% get the name of subfolders in FolderToCloned
+
+for i = 3 : length(filenames)
+      new =strcat(NewFolder,'\',filenames{i});
+      mkdir (new); cd(new);
+      %mkdir('001');mkdir('002');%create sub-subfolders
+end
+
+display('done');
